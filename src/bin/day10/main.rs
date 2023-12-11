d676e306ff2755c99d5b97cf7b50994458892a82
Alexis Delhelle <adelhelle@hellowork.com>
2023-12-11 12:01:17 +0100

    day10 p2

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("parsing took : {:?}", start.elapsed());
 
     let start = Instant::now();
-    part1(input);
+  //  part1(input);
     println!("day1 took : {:?}", start.elapsed());
 
     let start = Instant::now();
@@ -40,24 +40,51 @@ fn part1(input: &str) {
 
          };
      }
-
-
-
     println!("{:?}", max);
-    for i in 0..nodes.len() {
-        for j in 0..nodes.len() {
-            let a = if nodes[i][j].visited {
-                "#"
-            } else {
-                "."
-            };
-            print!("{a}" );
-        }
-        println!();
-    }
 }
 fn part2(input: &str) {
 
+    let mut mapped = parse(input);
+    let start = mapped.start;
+    let mut nodes = &mut mapped.nodes;
+    let mut next_nodes : VecDeque<Node>= VecDeque::with_capacity(140);
+    let mut max = 0;
+    let mut start = nodes[start.0 as usize][(start.1) as usize].clone();
+
+    next_nodes.push_back( start);
+    while let Some(current) = next_nodes.pop_front() {
+        for cur_dir in &current.directions {
+            let  next_node = &mut nodes[(current.pos.0 + cur_dir.dir.0) as usize][(current.pos.1 + cur_dir.dir.1) as usize];
+
+            let a = &next_node.directions;
+            for next_dir in a {
+                if cur_dir.can_go_to(next_dir) && (!next_node.visited || next_node.length > current.length + 1) {
+                    next_node.length = current.length + 1;
+                    max = max.max(next_node.length);
+                    next_node.visited = true;
+                    next_nodes.push_back(next_node.clone());
+                }
+            };
+
+        };
+    }
+    let wall = vec!['|','F', '7']; // 'S' in my input is a 'L' otherwise i add it for example
+    let mut count = 0;
+    for i in 0..nodes.len() {
+        let mut inside = false;
+        for j in 0..nodes[i].len() {
+            let a = &nodes[i][j];
+            if a.visited {
+                inside ^= wall.contains(&a.car);
+            } else {
+                if inside {
+                    count = count + 1;
+                }
+            }
+
+        }
+    }
+    println!("{count}");
 }
 
 fn parse(input: &str) -> Map {
@@ -78,7 +105,7 @@ fn parse(input: &str) -> Map {
                 _ => panic!("holy shit wtf")
             };
             // vec![Dir{dir: (1, 0 )},Dir{dir: (-1, 0)},Dir{dir: (0, 1)},Dir{dir: (0, -1)}]
-            line_map.push(Node {visited: current == 'S' , directions , length: 0, pos: (all_mapped.len() as isize, line_map.len() as isize)});
+            line_map.push(Node {visited: current == 'S' , directions , length: 0, pos: (all_mapped.len() as isize, line_map.len() as isize), car : current});
         });
         all_mapped.push(line_map);
     });
@@ -91,7 +118,8 @@ struct Node {
     visited: bool,
     directions: Vec<Dir>,
     length: isize,
-    pos: (isize, isize)
+    pos: (isize, isize),
+    car: char
 }
 #[derive(Debug, Clone)]
 struct Map {
